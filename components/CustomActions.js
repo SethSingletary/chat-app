db999c8aeb2f7a199f900ad13b3812e388f11045
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-08-13 19:25:28 -0400

    mostly working, says userID doesn't exist

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -4,6 +4,7 @@ import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps'; 
+import { getStorage } from 'firebase/storage';
 
 const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage}) => {
     const actionSheet = useActionSheet();
@@ -31,6 +32,12 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage}) => {
         );
       };
 
+      const generateReference = (uri) => {
+        const timeStamp = (new Date()).getTime();
+        const imageName = uri.split("/")[uri.split("/").length - 1];
+        return `${userID}-${timeStamp}-${imageName}`;
+      }
+
       const getLocation = async () => {
         let permissions = await Location.requestForegroundPermissionsAsync();
         if (permissions?.granted) {
@@ -45,22 +52,40 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage}) => {
           } else Alert.alert("Error occurred while fetching location");
         } else Alert.alert("Permissions haven't been granted.");
       }
+
+
+
+
+      const uploadAndSendImage = async (imageURI) => {
+        const uniqueRefString = generateReference(imageURI);
+        const newUploadRef = ref(storage, uniqueRefString);
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+          const imageURL = await getDownloadURL(snapshot.ref)
+          onSend({ image: imageURL })
+        });
+      }
+    
       const pickImage = async () => {
         let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (permissions?.granted) {
           let result = await ImagePicker.launchImageLibraryAsync();
-          if (!result.canceled) {
-            const imageURI = result.assets[0].uri;
-            const response = await fetch(imageURI);
-            const blob = await response.blob();
-            const newUploadRef = ref(storage, 'image123');
-            uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-              console.log('File has been uploaded successfully');
-            }) 
-          }
+          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
           else Alert.alert("Permissions haven't been granted.");
         }
       }
+    
+      const takePhoto = async () => {
+        let permissions = await ImagePicker.requestCameraPermissionsAsync();
+        if (permissions?.granted) {
+          let result = await ImagePicker.launchCameraAsync();
+          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+          else Alert.alert("Permissions haven't been granted.");
+        }
+      }
+
+
 
 
     return(
